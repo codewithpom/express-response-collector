ce4c89c334dfaae2dfc5923e37e2e7234e82f31c
Padmashree Jha <codewithpom@gmail.com>
2021-11-04 06:07:54 +0000

    Added back-end

---
+++ index.js
@@ -0,0 +1,37 @@
+// Here we will create an express app to collect responses from a response and save them in a csv file using the csv-append module
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+// add body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// create a post route to collect the responses at /submit in the response.csv file with time according to India's timezone in formatted date and time
+app.post('/submit', async (req, res) => {
+    const time = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+    console.log(
+        req.body
+    );
+    const data = [
+        `"${req.body.name}"`,
+        `"${req.body.email}"`,
+        `"${req.body.message}"`,
+        `"${time}"`
+    ];
+    // append the data to the csv file
+    fs.appendFile('response.csv', data.join(',') + '\n', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+    res.send('Thank you for your response!');
+});
+
+// start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
